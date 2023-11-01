23d0c39341cd811a72ec2dfbe2cd1805cac36ff1
LucasBucaiDev <135075398+LucasBucai@users.noreply.github.com>
2023-11-01 20:56:55 -0300

    Implementacion de user server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+
+const routerUsers = require("./router/users");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use("/api/users", routerUsers);
+
+const PORT = 5432;
+const server = app.listen(PORT, () =>
+  console.log(`Servidor express escuchando en el puerto: ${PORT}`)
+);
+server.on("error", (error) =>
+  console.log(`Error en servidor: ${error.message}`)
+);
